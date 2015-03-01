362633869d4172f662b7c90466c3773505b732e3
Wes Turner <wes@wrd.nu>
2015-02-28 18:59:25 -0600

    TST: test_read_local_data tests/data/local/*.ttl

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,9 @@
 
+import logging
 import os
 import unittest
+
+import rdflib
 
 import whereto.data as data
 
@@ -15,6 +18,16 @@ class TestWhereToData(unittest.TestCase):
         filepath = get_path('..', 'whereto', 'ns', 'whereto.ttl')
         output = data.read_data(filepath)
         self.assertTrue(output)
-        import rdflib
         self.assertIsInstance(output, rdflib.Graph)
         self.assertGreater(len(output), 0)
+
+    def test_read_local_data(self):
+        filenames = [get_path('data', 'local', x) for x in
+                     os.listdir(get_path('data', 'local'))
+                     if x.endswith('.ttl')]
+        for filepath in filenames:
+            logging.info(filepath)
+            output = data.read_data(filepath)
+            self.assertTrue(output)
+            self.assertIsInstance(output, rdflib.Graph)
+            self.assertGreater(len(output), 0)
